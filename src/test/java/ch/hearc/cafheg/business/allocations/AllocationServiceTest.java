1fea847d1b40357183172a832d166bacb884916f
lonix26 <nilo.castillo02@outlook.com>
2025-05-14 10:38:51 +0200

    refactor AllocationServiceTest for improved readability and structure

--- src/test/java/ch/hearc/cafheg/business/allocations/AllocationServiceTest.java
+++ src/test/java/ch/hearc/cafheg/business/allocations/AllocationServiceTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import ch.hearc.cafheg.business.common.Montant;
 import ch.hearc.cafheg.infrastructure.persistance.AllocataireMapper;
 import ch.hearc.cafheg.infrastructure.persistance.AllocationMapper;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.*;
@@ -16,79 +17,115 @@ import org.mockito.Mockito;
 
 class AllocationServiceTest {
 
-  private AllocationService allocationService;
+    private AllocationService allocationService;
 
-  private AllocataireMapper allocataireMapper;
-  private AllocationMapper allocationMapper;
+    private AllocataireMapper allocataireMapper;
+    private AllocationMapper allocationMapper;
 
-  @BeforeEach
-  void setUp() {
-    allocataireMapper = Mockito.mock(AllocataireMapper.class);
-    allocationMapper = Mockito.mock(AllocationMapper.class);
+    @BeforeEach
+    void setUp() {
+        allocataireMapper = Mockito.mock(AllocataireMapper.class);
+        allocationMapper = Mockito.mock(AllocationMapper.class);
 
-    allocationService = new AllocationService(allocataireMapper, allocationMapper);
-  }
+        allocationService = new AllocationService(allocataireMapper, allocationMapper);
+    }
 
-  @Test
-  void findAllAllocataires_GivenEmptyAllocataires_ShouldBeEmpty() {
-    Mockito.when(allocataireMapper.findAll("Geiser")).thenReturn(Collections.emptyList());
-    List<Allocataire> all = allocationService.findAllAllocataires("Geiser");
-    assertThat(all).isEmpty();
-  }
+    @Test
+    void findAllAllocataires_GivenEmptyAllocataires_ShouldBeEmpty() {
+        Mockito.when(allocataireMapper.findAll("Geiser")).thenReturn(Collections.emptyList());
+        List<Allocataire> all = allocationService.findAllAllocataires("Geiser");
+        assertThat(all).isEmpty();
+    }
 
-  @Test
-  void findAllAllocataires_Given2Geiser_ShouldBe2() {
-    Mockito.when(allocataireMapper.findAll("Geiser"))
-        .thenReturn(Arrays.asList(new Allocataire(new NoAVS("1000-2000"), "Geiser", "Arnaud"),
-            new Allocataire(new NoAVS("1000-2001"), "Geiser", "Aurélie")));
-    List<Allocataire> all = allocationService.findAllAllocataires("Geiser");
-    assertAll(() -> assertThat(all.size()).isEqualTo(2),
-        () -> assertThat(all.get(0).getNoAVS()).isEqualTo(new NoAVS("1000-2000")),
-        () -> assertThat(all.get(0).getNom()).isEqualTo("Geiser"),
-        () -> assertThat(all.get(0).getPrenom()).isEqualTo("Arnaud"),
-        () -> assertThat(all.get(1).getNoAVS()).isEqualTo(new NoAVS("1000-2001")),
-        () -> assertThat(all.get(1).getNom()).isEqualTo("Geiser"),
-        () -> assertThat(all.get(1).getPrenom()).isEqualTo("Aurélie"));
-  }
+    @Test
+    void findAllAllocataires_Given2Geiser_ShouldBe2() {
+        Mockito.when(allocataireMapper.findAll("Geiser"))
+                .thenReturn(Arrays.asList(new Allocataire(new NoAVS("1000-2000"), "Geiser", "Arnaud"),
+                        new Allocataire(new NoAVS("1000-2001"), "Geiser", "Aurélie")));
+        List<Allocataire> all = allocationService.findAllAllocataires("Geiser");
+        assertAll(() -> assertThat(all.size()).isEqualTo(2),
+                () -> assertThat(all.get(0).getNoAVS()).isEqualTo(new NoAVS("1000-2000")),
+                () -> assertThat(all.get(0).getNom()).isEqualTo("Geiser"),
+                () -> assertThat(all.get(0).getPrenom()).isEqualTo("Arnaud"),
+                () -> assertThat(all.get(1).getNoAVS()).isEqualTo(new NoAVS("1000-2001")),
+                () -> assertThat(all.get(1).getNom()).isEqualTo("Geiser"),
+                () -> assertThat(all.get(1).getPrenom()).isEqualTo("Aurélie"));
+    }
 
-  @Test
-  void findAllocationsActuelles() {
-    Mockito.when(allocationMapper.findAll())
-        .thenReturn(Arrays.asList(new Allocation(new Montant(new BigDecimal(1000)), Canton.NE,
-            LocalDate.now(), null), new Allocation(new Montant(new BigDecimal(2000)), Canton.FR,
-            LocalDate.now(), null)));
-    List<Allocation> all = allocationService.findAllocationsActuelles();
-    assertAll(() -> assertThat(all.size()).isEqualTo(2),
-        () -> assertThat(all.get(0).getMontant()).isEqualTo(new Montant(new BigDecimal(1000))),
-        () -> assertThat(all.get(0).getCanton()).isEqualTo(Canton.NE),
-        () -> assertThat(all.get(0).getDebut()).isEqualTo(LocalDate.now()),
-        () -> assertThat(all.get(0).getFin()).isNull(),
-        () -> assertThat(all.get(1).getMontant()).isEqualTo(new Montant(new BigDecimal(2000))),
-        () -> assertThat(all.get(1).getCanton()).isEqualTo(Canton.FR),
-        () -> assertThat(all.get(1).getDebut()).isEqualTo(LocalDate.now()),
-        () -> assertThat(all.get(1).getFin()).isNull());
-  }
+    @Test
+    void findAllocationsActuelles() {
+        Mockito.when(allocationMapper.findAll())
+                .thenReturn(Arrays.asList(new Allocation(new Montant(new BigDecimal(1000)), Canton.NE,
+                        LocalDate.now(), null), new Allocation(new Montant(new BigDecimal(2000)), Canton.FR,
+                        LocalDate.now(), null)));
+        List<Allocation> all = allocationService.findAllocationsActuelles();
+        assertAll(() -> assertThat(all.size()).isEqualTo(2),
+                () -> assertThat(all.get(0).getMontant()).isEqualTo(new Montant(new BigDecimal(1000))),
+                () -> assertThat(all.get(0).getCanton()).isEqualTo(Canton.NE),
+                () -> assertThat(all.get(0).getDebut()).isEqualTo(LocalDate.now()),
+                () -> assertThat(all.get(0).getFin()).isNull(),
+                () -> assertThat(all.get(1).getMontant()).isEqualTo(new Montant(new BigDecimal(2000))),
+                () -> assertThat(all.get(1).getCanton()).isEqualTo(Canton.FR),
+                () -> assertThat(all.get(1).getDebut()).isEqualTo(LocalDate.now()),
+                () -> assertThat(all.get(1).getFin()).isNull());
+    }
 
-  @Test
-  void givenParent1HasActivityAndParent2DoesNot_whenGetParentDroitAllocation_thenReturnParent1() {
-    Map<String, Object> params = new HashMap<>();
-    params.put("parent1ActiviteLucrative", true);
-    params.put("parent2ActiviteLucrative", false);
+    @Test
+    void givenParent1HasActivityAndParent2DoesNot_whenGetParentDroitAllocation_thenReturnParent1() {
+        Map<String, Object> params = new HashMap<>();
+        params.put("parent1ActiviteLucrative", true);
+        params.put("parent2ActiviteLucrative", false);
 
-    String result = allocationService.getParentDroitAllocation(params);
+        String result = allocationService.getParentDroitAllocation(params);
 
-    assertThat(result).isEqualTo("Parent1");
-  }
+        assertThat(result).isEqualTo("Parent1");
+    }
 
-  @Test
-  void givenParent2HasActivityAndParent1DoesNot_whenGetParentDroitAllocation_thenReturnParent2() {
-    Map<String, Object> params = new HashMap<>();
-    params.put("parent1ActiviteLucrative", false);
-    params.put("parent2ActiviteLucrative", true);
+    @Test
+    void givenParent2HasActivityAndParent1DoesNot_whenGetParentDroitAllocation_thenReturnParent2() {
+        Map<String, Object> params = new HashMap<>();
+        params.put("parent1ActiviteLucrative", false);
+        params.put("parent2ActiviteLucrative", true);
 
-    String result = allocationService.getParentDroitAllocation(params);
+        String result = allocationService.getParentDroitAllocation(params);
 
-    assertThat(result).isEqualTo("Parent2");
-  }
+        assertThat(result).isEqualTo("Parent2");
+    }
+
+    @Test
+    void givenBothParentsHaveActivityAndParent1HasHigherSalary_whenGetParentDroitAllocation_thenReturnParent1() {
+        Map<String, Object> params = new HashMap<>();
+        params.put("parent1ActiviteLucrative", true);
+        params.put("parent2ActiviteLucrative", true);
+        params.put("parent1Salaire", 3500);
+        params.put("parent2Salaire", 3000);
+
+        String result = allocationService.getParentDroitAllocation(params);
+
+        assertThat(result).isEqualTo("Parent1");
+    }
+    @Test
+    void givenBothParentsHaveActivityAndParent2HasHigherSalary_whenGetParentDroitAllocation_thenReturnParent2() {
+        Map<String, Object> params = new HashMap<>();
+        params.put("parent1ActiviteLucrative", true);
+        params.put("parent2ActiviteLucrative", true);
+        params.put("parent1Salaire", 3000);
+        params.put("parent2Salaire", 3500);
+
+        String result = allocationService.getParentDroitAllocation(params);
+
+        assertThat(result).isEqualTo("Parent2");
+    }
+    @Test
+    void givenBothParentsHaveActivityAndEqualSalaries_whenGetParentDroitAllocation_thenReturnParent2() {
+        Map<String, Object> params = new HashMap<>();
+        params.put("parent1ActiviteLucrative", true);
+        params.put("parent2ActiviteLucrative", true);
+        params.put("parent1Salaire", 3000);
+        params.put("parent2Salaire", 3000);
+
+        String result = allocationService.getParentDroitAllocation(params);
+        assertThat(result).isEqualTo("Parent2");
+    }
 
 }
